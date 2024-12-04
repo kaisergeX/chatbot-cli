3940e3b30dab9f22505e85f110ee9a13a8a96da5
kaisergeX <khaivd98@gmail.com>
2024-12-04 12:48:42 +0700

    feat: prompt rate limit

--- constants.ts
+++ constants.ts
@@ -1,9 +1,26 @@
 import "@std/dotenv/load";
 export const LOG_FILE = "chatbot_log.txt";
 
-export const ENGINE = {
-  STRICT_MODE: Deno.env.get("ENGINE_STRICT_MODE") === "off" ? false : true,
+const ENV_ENGINE = {
+  STRICT_MODE: Deno.env.get("ENGINE_STRICT_MODE") !== "off",
+  PROMT: Object.freeze({
+    /** Rate limit for the chatbot. Falsy value means no rate limit (eg: `NaN` and `0`). */
+    RATE_LIMIT: parseInt(Deno.env.get("PROMT_RATE_LIMIT") || "5"),
+    /**
+     * Rate limit period in milliseconds. This is the time window for the rate limit.
+     * Will be ignored if `RATE_LIMIT` is falsy.
+     */
+    RATE_LIMIT_PERIOD: parseInt(
+      Deno.env.get("PROMT_RATE_LIMIT_PERIOD") || "30000", // 30 seconds
+    ),
+  }),
 };
+
+export const ENGINE = Object.defineProperty(ENV_ENGINE, "PROMT", { // only make PROMT read-only
+  writable: false, // read-only
+  configurable: false,
+  enumerable: true,
+});
 
 export const RESTRICTED_WORDS = Deno.env.get("RESTRICTED_WORDS")?.split(",") ||
   [];
