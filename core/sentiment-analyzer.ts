35c0ac42a84278cf9785a906266be549c95da17e
kaisergeX <khaivd98@gmail.com>
2024-12-18 13:28:19 +0700

    chore: README

--- core/sentiment-analyzer.ts
+++ core/sentiment-analyzer.ts
@@ -8,6 +8,12 @@ const analyzer = new Natural.SentimentAnalyzer(
 
 const wordTokenizer = new Natural.WordTokenizer();
 
+/**
+ * Get the sentiment score of the content.
+ * - With `afinn` lexicon, the score ranges from -5 (most negative) to 5 (most positive).
+ * - With `senticon` lexicon, the score ranges from -1 (most negative) to 1 (most positive).
+ * @returns The sentiment score of the content.
+ */
 export function getSentimentScore(content: string): number {
   return analyzer.getSentiment(wordTokenizer.tokenize(content));
 }
